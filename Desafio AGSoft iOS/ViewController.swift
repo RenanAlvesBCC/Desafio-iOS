5baab77a102900a88122e06f30d1f7a980e3c21b
Yago Teixeira (Lord Horacio) <yagaot@gmail.com>
2019-02-05 15:22:56 -0200

    Adicionado nome dos aliens

--- Desafio AGSoft iOS/ViewController.swift
+++ Desafio AGSoft iOS/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    static let nomes = ["Prugue Seska", "Brellar Mara", "Hoggue Goro", "Aabran Benia", "Pretav Zara", "Shoggue Rejar", "Brony Maro Brony Parnia", "Tekor Damar", "Corbin Daro", "Siloc Tajor", "Aarmar Mosolmak Daro", "Kamem Seska", "Kovor Rusol", "Aaral Telle", "Crugue Mavok", "Corbin Pritza", "Meken Gara", "Kamad Daro", "Jori Trale", "Cretav Tania", "Anpat Padal", "Gavr", "Gaar", "Silon Pora", "Thavand sh'Share", "Odas sh'Zula", "Thanthaa zh'Rada", "Shrani sh'Vranthi", "Thrib zh'Shendria"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
